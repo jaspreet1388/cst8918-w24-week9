88c25f9a1ac57eded67b1b68aa72b340c0783ae2
jaspreet1388 <chha0038@algonquinlive.com>
2025-07-04 22:43:05 -0400

    fixed the file

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,15 +1,14 @@
 variable "location" {
+  type    = string
   default = "East US"
 }
 
 variable "admin_username" {
+  type    = string
   default = "azureuser"
 }
 
 variable "admin_password" {
+  type    = string
   default = "P@ssword1234!"  # Use env vars or Key Vault in production
 }
-
-asjgkjhzxbjhbdsjasdffdfdfddfddsdffdfdfdfznbdbdsnb
-variable "sdfd,jbjdfjkfd"
-{
